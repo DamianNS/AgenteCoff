8649be6080f8591bd864d17eb97b3076d5412d6d
Damián Saint Paul <53456589+DamianNS@users.noreply.github.com>
2026-07-23 23:43:48 -0300

    Agrego Webhook

---
+++ AgenteCoff.ApiService/Controllers/WebhookController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgenteCoff.ApiService.Controllers
+{
+    [Route("api/Webhook")]
+    [ApiController]
+    public class WebhookController : ControllerBase
+    {
+        public class WebhookPayload
+        {
+            public string titulo { get; set; }
+            public string body { get; set; }
+        }
+
+        [HttpPost]
+        public IActionResult Post(WebhookPayload payload)
+        {
+            Console.WriteLine(payload.titulo);
+            Console.WriteLine(payload.body);
+            return Ok(new { message = "Webhook received successfully", data = payload });
+        }
+    }
+}
